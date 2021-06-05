d93353e4a11229c5743c2e9d557c1e80284e8d81
MikhailinNik <78532457+MikhailinNik@users.noreply.github.com>
2021-06-05 11:40:23 +0300

    Исправил замечания наставника

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5
 const getRandomNumber = (min, max) => {
   if (min < 0 || max < 0) {
-    return RangeError('One of parametr could not less 0');
+    throw new RangeError('One of parametr could not less 0');
   }
 
   min = Math.ceil(min);
@@ -14,7 +14,7 @@ const getRandomNumber = (min, max) => {
 // Задание №2
 const getRandomNumberFloat = (min, max, digits) => {
   if (min < 0 || max < 0) {
-    return RangeError('One of parametr could not less 0');
+    throw new RangeError('One of parametr could not less 0');
   }
 
   min = Math.ceil(min);
