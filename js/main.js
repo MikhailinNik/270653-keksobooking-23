64ac180c546ae663f23d02c10e659c6d72492f7a
MikhailinNik <78532457+MikhailinNik@users.noreply.github.com>
2021-06-06 20:02:42 +0300

    Выполнил задание №8

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5
 const getRandomNumber = (min, max) => {
   if (min < 0 || max < 0) {
-    return RangeError('One of parametr could not less 0');
+    throw new RangeError('One of parametr could not less 0');
   }
 
   min = Math.ceil(min);
@@ -14,11 +14,11 @@ const getRandomNumber = (min, max) => {
 // Задание №2
 const getRandomNumberFloat = (min, max, digits) => {
   if (min < 0 || max < 0) {
-    return RangeError('One of parametr could not less 0');
+    throw new RangeError('One of parametr could not less 0');
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  // min = Math.ceil(min);
+  // max = Math.floor(max);
 
   return (Math.random() * (max - min) + min).toFixed(digits);
 };
@@ -26,6 +26,74 @@ const getRandomNumberFloat = (min, max, digits) => {
 getRandomNumber(1, 12);
 getRandomNumberFloat(3, 10, 3);
 
-// 1. Добавил стрелочные функции
-// 2. Изменил названием 3-его параметра во второй функции
-// 3. Добавил каждой функции в условие исключение RangeError
+
+// Задание №8
+const TITLES = [
+  'Офис',
+  '2-ух комнатная квартира',
+  '3-ех комнатная квартира',
+  'Однокомнатная квартира',
+  'Помещение для аренды в торговом центре'];
+
+const TYPES = [
+  'palace', 'flat', 'house', 'bunglow', 'hotel'];
+
+const CHECKS = [
+  '12:00', '13:00', '14:00'];
+
+const FEATURES = [
+  'wifi', 'dishwasher',
+  'parking', 'washer',
+  'elevator', 'conditioner'];
+
+const DESCRIPTIONS = [
+  'кухня',
+  'ванная',
+  'гостинная',
+  'туалет',
+  'спальня'];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const getRandomArrayElements = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const createAuthor = () => {
+  const randomNumber = getRandomNumber(1, 8);
+  return { avatar: `img/avatars/user0${randomNumber}.png`};
+};
+
+const createOffer = () =>
+  ({
+    title: getRandomArrayElements(TITLES),
+    address: `${getRandomNumber(0, 100)}, ${getRandomNumber(0, 100)}`,
+    price: getRandomNumber(1, 1000),
+    types: getRandomArrayElements(TYPES),
+    rooms: getRandomNumber(1, 5),
+    guests: getRandomNumber(1, 15),
+    checkin: getRandomArrayElements(CHECKS),
+    checkout: getRandomArrayElements(CHECKS),
+    features: FEATURES.filter(() => Boolean(getRandomNumber(0, 1))),
+    description: getRandomArrayElements(DESCRIPTIONS),
+    photos: PHOTOS.filter(() => Boolean(getRandomNumber(0, 1)))}
+  );
+
+const createLocation = () =>
+  ({
+    lat: getRandomNumberFloat(35.65000, 35.70000, 5),
+    lng: getRandomNumberFloat(139.70000, 139.80000, 5)}
+  );
+
+const createObject = () =>
+  ({
+    author: createAuthor(),
+    offer: createOffer(),
+    location: createLocation()}
+  );
+
+const similarObjectCount = (count) => count;
+
+const similarObject = new Array(similarObjectCount(10)).fill(null).map(() => createObject());
+similarObject;
