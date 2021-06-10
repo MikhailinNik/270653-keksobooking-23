db2f6e0d8d8f098a53e89fb82a86f7251331c03d
MikhailinNik <78532457+MikhailinNik@users.noreply.github.com>
2021-06-10 15:48:18 +0300

    Внес изменения №4

--- js/main.js
+++ js/main.js
@@ -60,14 +60,12 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const locationRandomLat = getRandomNumberFloat(35.65000, 35.70000, 5);
-const locationRandomLng = getRandomNumberFloat(139.70000, 139.80000, 5);
 
 const getRandomArrayItem = (items) => items[getRandomNumber(0, items.length - 1)];
 
-const createOffer = () => ({
+const createOffer = (coord1, coord2) => ({
   title: getRandomArrayItem(TITLES),
-  address: `${locationRandomLat}, ${locationRandomLng}`,
+  address: `${coord1}, ${coord2}`,
   price: getRandomNumber(1, 1000),
   types: getRandomArrayItem(TYPES),
   rooms: getRandomNumber(1, 5),
@@ -77,12 +75,6 @@ const createOffer = () => ({
   features: FEATURES.filter(() => Boolean(getRandomNumber(0, 1))),
   description: getRandomArrayItem(DESCRIPTIONS),
   photos: PHOTOS.filter(() => Boolean(getRandomNumber(0, 1))),
-});
-
-
-const createLocation = () => ({
-  lat: locationRandomLat,
-  lng: locationRandomLng,
 });
 
 const creatAuthor = (id) => {
@@ -97,11 +89,19 @@ const creatAuthor = (id) => {
   });
 };
 
-const createAdvert = (id) => ({
-  author: creatAuthor(id),
-  offer: createOffer(),
-  location: createLocation(),
-});
+const createAdvert = (id) => {
+  const latitude = getRandomNumberFloat(35.65000, 35.70000, 5);
+  const langtitude = getRandomNumberFloat(139.70000, 139.80000, 5);
+
+  return ({
+    author: creatAuthor(id),
+    offer: createOffer(latitude, langtitude),
+    location: {
+      lat: latitude,
+      lng: langtitude,
+    },
+  });
+};
 
 const adverts = Array.from({ length: 10 }, (value, idx) => createAdvert(idx + 1));
 adverts;
